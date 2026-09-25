440e85422539af2a96c8bd70d4a560364bf7947b
rlawjddnr <95038242+DRJungOok@users.noreply.github.com>
2026-09-25 11:33:50 +0000

    add files and folders

--- demo/src/main/java/controller/IndexController.java
+++ demo/src/main/java/controller/IndexController.java
@@ -1,12 +1,14 @@
-package com.example.demo;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class IndexController {
+
+    // Main Page
     @GetMapping("/")
     public String test() {
-        return "test.html";
+        return "index.html";
     }
 }
---
+++ demo/src/main/java/controller/SignOnController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class SignOnController {
+    @GetMapping("/signOn")
+    public String signOn() {
+        return "signOn.html";
+    }
+    
+}
